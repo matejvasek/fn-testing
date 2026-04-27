0f30b26041a6e53e097c7948eb3534af2bc3fb08
Matej Vašek <matejvasek@gmail.com>
2026-04-27 18:01:27 +0200

    Update function files

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const handle = async (context, body) => {
   if (context.method === 'POST') {
     return { body };
   } else if (context.method === 'GET') {
+    context.log.info('abc')
     // If the request is an HTTP GET, the context will include a query string, if it exists
     return {
       query: context.query,
